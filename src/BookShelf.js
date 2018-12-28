896fd376ceca552b1e1c784c68f1ea4f55385236
Sofia Vistas <sofia.vistas@catenamedia.com>
2018-12-28 13:44:52 +0200

    type: feat
    Add BookShelf to hold BookItems

---
+++ src/BookShelf.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import BookItem from './BookItem';
+
+class BookShelf extends Component {
+  render() {
+    const { books, shelf, title } = this.props;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2> //need data here
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+          {books.filter((book) => book.shelf === shelf).map((book) =>
+            <li>
+              <BookItem book={book}/>
+            </li>
+          )}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookShelf;
