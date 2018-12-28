6815d806efbf8a7670cc444cad6d2de85b647ec9
Sofia Vistas <sofia.vistas@catenamedia.com>
2018-12-28 13:57:36 +0200

    type: feat
    Add BookShelves to hold all shelves available

---
+++ src/BookShelves.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import BookShelf from './BookShelf';
+
+class BookShelves extends Component {
+  render() {
+    const { allBooks } = this.props;
+
+    render(
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf books={allBooks} title={"Currently Reading"} shelf={"currentlyReading"}>
+          </div>
+          <div>
+            <BookShelf books={allBooks} title={"Want To Read"} shelf={"wantToRead"}>
+          </div>
+          <div>
+            <BookShelf books={allBooks} title={"Read"} shelf={"read"}>
+          </div>
+        <div>
+        <div className="open-search">
+          <Link to="/search">Add a book<Link>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default BookShelves;
